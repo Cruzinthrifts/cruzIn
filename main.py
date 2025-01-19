f544c5105716d540c056deef2fad9bd9740933c7
Cruzerman <cbmcruzerman@hotmail.com>
2025-01-19 12:49:32 -0800

    changes

---
+++ main.py
@@ -0,0 +1,69 @@
+
+import time
+#testing python
+
+first_name = "Cruz"
+food = "pizza"
+email = "bbb@yahoo.com"
+age = 25
+
+#stings
+print(first_name)
+
+#stings and integer
+print(f"hello {first_name} you are {age} and love to eat {food}")
+
+#float
+height = 6.2
+gpa = 4.2
+#boolean
+is_student = False
+
+if is_student:
+    print("what are you studing.")
+else:
+    print("good for you")
+
+    #how to print type cast functions
+
+gpa = int(gpa)
+
+print(type(gpa))
+
+print(gpa)
+
+#while 1==1 :
+    #print("help Im stuck in a loop")#
+name = None
+#while not name:
+    #name = input("enter your name: ")
+
+    #print("hello "+name) 
+
+    #for loops 
+
+    #while loop = unlimted times
+    #for loop = limited times
+
+#for i in range(50,100,2):
+    # print(i)
+
+
+#for seconds in range (10,0,-1):
+       # print(seconds)
+      #  time.sleep(1)
+#print("Happy new Year!")
+
+
+######Nested loops#######
+
+#rows = int(input("how many rows: "))
+#columns = int(input("how many columns: "))
+#symbol = input("enter a symbol to use: ")
+
+#for i in range(rows):
+    #for j in range(columns):
+        #print(symbol, end="")
+    #print()
+
+    
